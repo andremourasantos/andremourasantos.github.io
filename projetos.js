d84c55671183eeb722adb9e37e7bdecc605b3cad
André Moura Santos <contato@andremourasantos.com>
2022-01-23 18:18:29 -0400

    Atualização da 5ª semana de Janeiro
    - Adição da página de Meus Projetos.
    - Adição de 7 projetos, com 5 descrições detalhadas.
    - Adição da seção "Projetos" na página inicial.
    - Correação de pequenos termos relacionados entre páginas.
    - Pequenas correções ortográficas.

---
+++ projetos.js
@@ -0,0 +1,68 @@
+const popupContainer = document.getElementById('popupcontainer')
+const html = document.querySelector('html')
+
+// ↓ POPUPS ↓
+function abrirContainerPopup() {
+    var nodes = document.getElementById('popupcontainer').childNodes;
+    for(var i=0; i<nodes.length; i++) {
+        if (nodes[i].nodeName.toLowerCase() == 'div') {
+            nodes[i].style.animation = '250ms linear abrirPopup';
+        }}
+    setTimeout(() => {
+        popupContainer.style.display = 'flex'
+        html.style.scrollBahavior = 'auto'
+        html.style.overflowY = 'hidden'
+    }, 250);
+}
+
+function fecharPopup() {
+    var nodes = document.getElementById('popupcontainer').childNodes;
+        for(var i=0; i<nodes.length; i++) {
+            if (nodes[i].nodeName.toLowerCase() == 'div') {
+                nodes[i].style.animation = '250ms linear fecharPopup';
+            }}
+    setTimeout(() => {
+        for(var i=0; i<nodes.length; i++) {
+            if (nodes[i].nodeName.toLowerCase() == 'div') {
+                nodes[i].style.display = 'none';
+            }}
+        popupContainer.style.display = 'none'
+        html.style.scrollBahavior = 'smooth'
+        html.style.overflowY = 'auto'
+    }, 250);
+}
+
+// ↓ SITE PROFISSIONAL ↓
+function siteProfissionalPopup() {
+    document.getElementById('siteProfissional').style.display = 'block'
+}
+
+// ↓ SITE DR. ANDRÉ ↓
+function siteDrAndréPopup() {
+    document.getElementById('siteDrAndré').style.display = 'block'
+}
+
+// ↓ REBRANDING DA BUSINESS SCHOOL ↓
+function rebrandingBSPopup() {
+    document.getElementById('rebrandingBS').style.display = 'block'
+}
+
+// ↓ UM ANDRÉ EM CURITIBA ↓
+function umAndreemCuritibaPopup() {
+    document.getElementById('umAndreemCuritiba').style.display = 'block'
+}
+
+// ↓ INTERVENÇÃO ARTÍSTICA ↓
+function intervençãoArtísticaPopup() {
+    document.getElementById('intervençãoArtística').style.display = 'block'
+}
+
+// ↓ WAVE ↓
+function wavePopup() {
+    document.getElementById('wave').style.display = 'block'
+}
+
+// ↓ EMCT ↓
+function ecmtPopup() {
+    document.getElementById('emct').style.display = 'block'
+}
