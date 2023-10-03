6a8835ca75c0440d99751b678b9addadb1442ebf
André Moura <contato@andremourasantos.com>
2023-10-03 18:05:07 -0300

    ALPHA 1.6 - Adicionado conteúdo na view PrivacyPolicy. - Adicionado mudança da tag title em router.ts. - Adicionado pp.json (abreviação de privacy policy) em /data/. - Alterações de SEO no HTML estático. - Alteradas as imagens de gradientes para as não otimizadas, devido à visível baixa qualidade visual das versões otimizadas. - Firebase inicializado no diretório.

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,28 +8,40 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
+    meta: {title: 'Página Inicial | André Moura Santos'},
     component: HomeView
   },
   {
     path: '/desenvolvimento-web',
     name: 'webDev',
+    meta: {title: 'Desenvolvimento Web'},
     component: WebDev
   },
   {
     path: '/marketing-digital',
     name: 'marketing',
+    meta: {title: 'Marketing Digital'},
     component: Marketing
   },
   {
     path: '/politica-de-privacidade',
     name: 'pp',
+    meta: {title: 'Política de Privacidade'},
     component: PrivacyPolicy
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return {top:0}
+  }
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title as string;
+  next();
 })
 
 export default router
--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -3,6 +3,15 @@
   <div id="gradient-top-right">
     <img src="../assets/purple.png">
   </div>
+  <section id="pp-section">
+    <article v-for="entry in PPJSON" :key="entry.title" class="pp-entry">
+      <div class="pp-entry-title">
+        <h3>{{ entry.title }}</h3>
+        <p>{{ entry.description }}</p>
+      </div>
+      <p v-for="content in entry.content" class="pp-entry-content">{{ content }}</p>
+    </article>
+  </section>
 </template>
 
 <script lang="ts">
@@ -11,15 +20,42 @@ import { defineComponent } from 'vue';
 //Components
 import PageTitle from '@/components/PageTitle.vue';
 
+//Data
+import PPJSON from '@/data/pp.json';
+
 export default defineComponent({
   components: {PageTitle},
   setup() {
-    
+    return {
+      PPJSON
+    }
   },
 })
 </script>
 
 
 <style scoped>
+  #pp-section {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    gap: 32px;
+    padding: 64px 0px 0px 0px;
+  }
 
+  .pp-entry-title {
+    margin-bottom: 16px;
+  }
+
+  .pp-entry-title h3 {
+    margin-bottom: 4px;
+  }
+
+  .pp-entry-title p {
+    font-weight: 600;
+  }
+
+  .pp-entry-content {
+    margin: 8px auto;
+  }
 </style>
