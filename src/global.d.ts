d9c2defbbbeb05a143f969b2aa68287440b4b7e8
André <contato@andremourasantos.com.br>
2025-05-24 18:57:35 -0300

    rebuilt-2025 v0.39 Atualizados componentes de botão e grupo de botões, adicionada lógica para que a pessoa se cadastre para receber uma notificação quando o artigo em rascunho estiver disponível e ajustados estilos para melhor usabilidade. Agora os artigos em rascunho também aparecem na produção.

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,4 +2,6 @@ export {}
 
 declare global {
   type IconName = "ChevronDown" | "ExternalLink" | "Link" | "Menu" | "WhatsApp" | "Facebook" | "LinkedIn" | "Twitter" | "Undo2" | "CalendarFold" | "SquareUser" | "Info" | "MessageSquareShare" | "X" | "BellPlus";
+
+  type ButtonSize = "S" | "M" | "L";
 }
