a014635ce179f6b44991ad463db62d8b72d1ff53
André <contato@andremourasantos.com.br>
2025-05-31 13:28:46 -0300

    rebuilt-2025 v0.47 Adicionado componente para formulário de newsletter e ajuste nos botões de inscrição.

--- src/composables/subscribeToArticleRelease.ts
+++ src/composables/subscribeToArticleRelease.ts
@@ -1,6 +1,6 @@
 async function subscribeToArticleRelease(event:Event, articleId:string, articleTitle:string) {
-  const targetButton = event.currentTarget as HTMLButtonElement;
-  const loadingStateButton = document.querySelector(`[data-btn-article-id="${articleId}"][data-btn="loadingSubscribToNewsletter"]`) as HTMLButtonElement;
+  const targetButton = Array.from(document.querySelectorAll(`[data-btn-article-id="${articleId}"][data-btn="subscribeToNewsletter"]`)) as HTMLButtonElement[];
+  const loadingStateButton = Array.from(document.querySelectorAll(`[data-btn-article-id="${articleId}"][data-btn="loadingSubscribeToNewsletter"]`)) as HTMLButtonElement[];
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   let email = window.prompt(`Insira seu endereço de e-mail para receber uma notificação quando o artigo "${articleTitle}" ficar pronto!\n\nE-mail:`);
 
@@ -10,27 +10,28 @@ async function subscribeToArticleRelease(event:Event, articleId:string, articleT
     return alert("Algo não parece certo...\nConfira o e-mail inserido e tente novamente.");
   }
 
-  targetButton.style.display = "none";
-  loadingStateButton.style.display = "flex";
-
-  let confirm = window.confirm(`Você receberá apenas a notificação referente a este artigo. Caso queria saber quando outros artigos forem ao ar, é necessário que se cadastre individualmente em cada um deles.\n\nAo confirmar o seu cadastro, você concorda em receber emails esporádicos sobre novidades por aqui.\nVocê pode conferir como seu e-mail é tratado na Política de Privacidade.`);
+  let confirm = window.confirm(`Você receberá apenas a notificação referente a este artigo. Caso queria saber quando outros artigos forem ao ar, é necessário que se cadastre individualmente em cada um deles.\n\nAo confirmar o seu cadastro, você concorda em receber emails esporádicos sobre novidades por aqui. Você pode conferir como seu e-mail é tratado na Política de Privacidade.`);
   if(confirm === false){
     return alert("Ok, nada foi feito.")
   }
 
   try {
+    targetButton.forEach(btn => {btn.style.display = "none"});
+    loadingStateButton.forEach(btn => {btn.style.display = "flex"});
+    
     const register = await fetch(`${import.meta.env.PUBLIC_WEBHOOK}?email=${encodeURI(email)}&articleId=${encodeURI(articleId)}&articleTitle=${encodeURI(articleTitle)}`);
+    
     if(!register.ok) {
       throw new Error();
-    }
+    };
   } catch (error) {
-    targetButton.style.display = "flex";
-    loadingStateButton.style.display = "none";
+    targetButton.forEach(btn => {btn.style.display = "flex"});
+    loadingStateButton.forEach(btn => {btn.style.display = "none"});
     return alert("Ocorreu um erro ao registrar o seu interesse, por favor, tente novamente.");
   }
 
-  loadingStateButton.style.display = "none";
-  targetButton.disabled = true;
+  loadingStateButton.forEach(btn => {btn.style.display = "none"});
+  targetButton.forEach(btn => {btn.disabled = true});
   saveArticleSubscriptionToLocalStorage(articleId);
   dispatchSubscriptionEvent(articleId);
   
@@ -66,7 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
     notifyMeBtn.forEach((btn) => {
       const btnEl = btn as HTMLButtonElement;
       subscriptionsArray.forEach(sub => {
-        if(btnEl.id === sub){
+        if(btnEl.dataset.btnArticleId === sub){
           dispatchSubscriptionEvent(sub);
           btnEl.disabled = true;
         };
@@ -78,7 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnEl = btn as HTMLButtonElement;
     if(!btnEl.disabled){
       btnEl.addEventListener("click", (event) => {
-        subscribeToArticleRelease(event, btnEl.id || "geral", btnEl.dataset.articleTitle || "Geral");
+        subscribeToArticleRelease(event, btnEl.dataset.btnArticleId || "geral", btnEl.dataset.articleTitle || "Geral");
       });
     };
   });
