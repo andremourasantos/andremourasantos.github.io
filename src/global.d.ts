b1a2bf307ce1d28e5111eac918b3dd1b61470004
andremourasantos <contato@andremourasantos.com.br>
2025-11-04 23:53:10 -0300

    versão 3.1.11 Atualizado texto "Aprimorando a inteligência de campanhas com um CRM", do portfólio, para "Alcançando um ROAS de 300% com conversões offline", agora em trabalho.

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,5 @@ export {}
 declare global {
   type IconName = "ChevronDown" | "ExternalLink" | "Link" | "Menu" | "WhatsApp" | "Facebook" | "LinkedIn" | "Twitter" | "Undo2" | "CalendarFold" | "SquareUser" | "Info" | "MessageSquareShare" | "X" | "BellPlus" | "Check" | "LoaderCircle";
   
-
   type ButtonSize = "S" | "M" | "L";
 }
